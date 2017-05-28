1c7f0df8d8d3904d1c4655c5461921a729e0cd7a
DESKTOP-G27KDOV\tharduz <isidroevc@gmail.com>
2017-05-27 20:39:38 -0500

    TestDisplay creado

---
+++ CajeroAutomatico/src/cajeroautomatico/Test/TestDisplay.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cajeroautomatico.Test;
+
+import cajeroautomatico.View.Display;
+
+/**
+ *
+ * @author tharduz
+ */
+public class TestDisplay {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Display d = new Display();
+        d.mostrar();
+    }
+    
+}
